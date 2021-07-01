da3feb22b1b0435634728ed7a8bf7d038d6e6c23
name <name@gmail.com>
2021-07-02 04:19:24 +0700

    teh

--- src/components/leftbar/Leftbar.jsx
+++ src/components/leftbar/Leftbar.jsx
@@ -30,9 +30,9 @@ const Leftbar = ({ children, open }) => {
     <div className="leftbar">
       <div className="leftbar-left">
         <Menu
-          style={{ width: "98%" }}
+          style={{ width: "98%", paddingTop: "10px" }}
           theme="dark"
-          defaultSelectedKeys={[`${open}`]}
+          defaultSelectedKeys={[1]}
         >
           <Menu.Item
             key="1"
@@ -83,93 +83,6 @@ const Leftbar = ({ children, open }) => {
             </Menu.Item>
           </SubMenu>
         </Menu>
-        {/* <div className="left-bar-text-container">
-          {" "}
-          <Link to="/beranda">
-            <div className="leftbar-text">
-              <i className="fas fa-home"></i>
-              <p>Beranda</p>
-              <i className="fas fa-chevron-right"></i>
-            </div>
-          </Link>
-        </div>
-        <div className="left-bar-text-container">
-          <div
-            className="leftbar-text"
-            onClick={() => setBar({ bar1: !bar.bar1 })}
-          >
-            <i className="fas fa-wallet"></i>
-            <p>Data Master</p>
-            <i
-              className="fas fa-chevron-right"
-              style={{
-                transform: bar.bar1 ? "rotate(90deg)" : "rotate(0)",
-                transitionDuration: "0.5s",
-              }}
-            ></i>
-          </div>
-          {bar.bar1 && (
-            <div className=" leftbar-child">
-              <Link to="/data-barang">
-                <p>Data Barang</p>
-              </Link>
-
-              <Link to="/return">
-                <p>Return</p>
-              </Link>
-            </div>
-          )}
-        </div>
-
-        <div className="left-bar-text-container">
-          <div
-            className="leftbar-text"
-            onClick={() => setBar({ bar2: !bar.bar2 })}
-          >
-            <i className="fas fa-clone"></i>
-            <p>transaksi</p>
-            <i
-              className="fas fa-chevron-right"
-              style={{
-                transform: bar.bar2 ? "rotate(90deg)" : "rotate(0)",
-                transitionDuration: "0.5s",
-              }}
-            ></i>
-          </div>
-          {bar.bar2 && (
-            <div className=" leftbar-child">
-              <Link to="/barang-masuk">
-                <p>Barang Masuk</p>
-              </Link>
-              <Link to="/barang-keluar">
-                <p>Barang Keluar</p>
-              </Link>
-            </div>
-          )}
-        </div>
-        <div className="left-bar-text-container">
-          <div
-            className="leftbar-text"
-            onClick={() => setBar({ bar3: !bar.bar3 })}
-          >
-            <i className="fas fa-file"></i>
-            <p>Laporan</p>
-            <i
-              className="fas fa-chevron-right"
-              style={{
-                transform: bar.bar3 ? "rotate(90deg)" : "rotate(0)",
-                transitionDuration: "0.5s",
-              }}
-            ></i>
-          </div>
-          {bar.bar3 && (
-            <div className=" leftbar-child">
-              <Link to="/stock-barang">
-                <p>Stock Barang</p>
-              </Link>
-            </div>
-          )}
-        </div> */}
       </div>
       <div className="leftbar-right">{children}</div>
     </div>
