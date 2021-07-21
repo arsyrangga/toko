058863dde2ab9fa84dd3bc4875aa0b60e9f4507e
name <name@gmail.com>
2021-07-21 18:32:16 +0700

    rangga

--- src/pages/barang-keluar/BarangKeluar.jsx
+++ src/pages/barang-keluar/BarangKeluar.jsx
@@ -4,11 +4,111 @@ import Navbar from "../../components/navbar/Navbar";
 import Leftbar from "../../components/leftbar/Leftbar";
 import { Link } from "react-router-dom";
 import Footer from "../../components/footer/Footer";
-import { Table, Tag, Space } from "antd";
+import { Table, Tag, Space, Input, Button } from "antd";
 import { useEffect, useState } from "react";
 import EditKeluar from "../../components/edit-keluar/EditKeluar";
+import Highlighter from "react-highlight-words";
+import { SearchOutlined } from "@ant-design/icons";
 
 function BarangKeluar() {
+  const [change, setChange] = useState({
+    searchText: "",
+    searchedColumn: "",
+    searchInput: "",
+  });
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={(node) => {
+            change.searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ marginBottom: 8, display: "block" }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setChange({
+                searchText: selectedKeys[0],
+                searchedColumn: dataIndex,
+              });
+            }}
+          >
+            Filter
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex]
+            .toString()
+            .toLowerCase()
+            .includes(value.toLowerCase())
+        : "",
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) {
+        setTimeout(() => change.searchInput.select(), 100);
+      }
+    },
+    render: (text) =>
+      change.searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+          searchWords={[change.searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ""}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setChange({
+      searchText: selectedKeys[0],
+      searchedColumn: dataIndex,
+    });
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setChange({ searchText: "" });
+  };
   const onEdit = (key, e) => {
     e.preventDefault();
     const data = barang.filter((item) => item.id == key);
@@ -54,14 +154,16 @@ function BarangKeluar() {
       key: "tanggal",
     },
     {
-      title: "Nama lengkap",
+      title: "Nama Barang",
       dataIndex: "nama",
       key: "nama",
+      ...getColumnSearchProps("nama"),
     },
     {
       title: "Kategori",
       dataIndex: "kategori",
       key: "kategori",
+      ...getColumnSearchProps("kategori"),
     },
     {
       title: "Merk",
